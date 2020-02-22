f1582e8cbc38482003958050bcea1500b8d15242
swonb <47047914+swonb@users.noreply.github.com>
2020-02-22 09:37:03 -0500

    Fixed Login Image

--- Maple-Building-Management-App/Controllers/HomeController.cs
+++ Maple-Building-Management-App/Controllers/HomeController.cs
@@ -88,45 +88,6 @@ namespace Maple_Building_Management_App.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Login(AccountModel model, string returnUrl)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(model);
-        //    }
-
-        //    // Require the user to have a confirmed email before they can log on.
-        //    var user = await UserManager.FindByNameAsync(model.EmailAddress);
-        //    if (user != null)
-        //    {
-        //        if (!await UserManager.IsEmailConfirmedAsync(user.Id))
-        //        {
-        //            ViewBag.errorMessage = "You must have a confirmed email to log on.";
-        //            return View("Error");
-        //        }
-        //    }
-
-        //    // This doesn't count login failures towards account lockout
-        //    // To enable password failures to trigger account lockout, change to shouldLockout: true
-        //    var result = await SignInManager.PasswordSignInAsync(model.EmailAddress, model.Password, shouldLockout: false);
-        //    switch (result)
-        //    {
-        //        case SignInStatus.Success:
-        //            return RedirectToLocal(returnUrl);
-        //        case SignInStatus.LockedOut:
-        //            return View("Lockout");
-        //        case SignInStatus.RequiresVerification:
-        //            return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-        //        case SignInStatus.Failure:
-        //        default:
-        //            ModelState.AddModelError("", "Invalid login attempt.");
-        //            return View(model);
-        //    }
-        //}
-
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
