53c4b8f264bc3e42865910a347e402ed040d29d0
Earl Denzel Perez <earldenzel@gmail.com>
2020-02-26 01:41:40 -0500

    added twilio package, and also identity entity framework started on 2fa

---
+++ Maple-Building-Management-App/App_Start/IdentityConfig.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Maple_Building_Management_App
+{
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Twilio Begin
+            var accountSid = ConfigurationManager.AppSettings["SMSAccountIdentification"];
+            var authToken = ConfigurationManager.AppSettings["SMSAccountPassword"];
+            var fromNumber = ConfigurationManager.AppSettings["SMSAccountFrom"];
+
+            TwilioClient.Init(accountSid, authToken);
+
+            MessageResource result = MessageResource.Create(
+            new PhoneNumber(message.Destination),
+            from: new PhoneNumber(fromNumber),
+            body: message.Body
+            );
+
+            //Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            Trace.TraceInformation(result.Status.ToString());
+            //Twilio doesn't currently have an async API, so return success.
+            return Task.FromResult(0);
+            // Twilio End
+
+            // ASPSMS Begin 
+            // var soapSms = new MvcPWx.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            // soapSms.SendSimpleTextSMS(
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"],
+            //   message.Destination,
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountFrom"],
+            //   message.Body);
+            // soapSms.Close();
+            // return Task.FromResult(0);
+            // ASPSMS End
+        }
+    }
+}
